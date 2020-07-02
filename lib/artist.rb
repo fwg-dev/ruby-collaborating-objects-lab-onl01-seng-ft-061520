c69e24b4ffc92cdd394790bb67664b5f5ebe7b07
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 07:11:35 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_accessor :name, :songs
+@@all = [] #stores all instances of Artist
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def add_song(song)
+    song.artist = self
+  end
+  #Artist.songs reader returns all songs
+  def songs
+    Song.all.select {|song|song.artist == self}
+  end
+
+  def self.find_or_create_by_name(name)
+ # self.find(name)? self.find(name) : self.new(name)
+ if self.find(name)
+         self.find(name)
+       else
+         self.create(name)
+       end
+
+  end
+
+end
